17e9f7105bff334f733c943371f3230f45cc4fa2
Georgiy <isak.georgiy@mail.ru>
2023-11-25 09:07:16 +0300

    i've replace repeat code

--- config_data/get_response.py
+++ config_data/get_response.py
@@ -6,36 +6,35 @@ from loader import bot
 from telebot.types import Message
 
 
+def get_current_film_info_from_command(current_film):
+    return (
+        current_film['title'],
+        current_film['year'],
+        current_film['rating'],
+        current_film['image'],
+        current_film['thumbnail'],
+        current_film['genre'],
+        current_film['description']
+    )
+
+
 def get_film_info(message: Message, from_module: str):
     response = requests.get(url=url, headers=headers)
     if response.status_code != 200:
         return False
+
     films = response.json()
     if from_module == 'low':
         """ Берем информацию для реализации команды /low """
         current_film_rate = min(films, key=lambda x: x['rating'])
-        film_name = current_film_rate['title']
-        film_year = current_film_rate['year']
-        film_rating = current_film_rate['rating']
-        url_image = current_film_rate['image']
-        film_thumbnail = current_film_rate['thumbnail']
-        film_genre = current_film_rate['genre']
-        film_description = current_film_rate['description']
-        return (film_name, film_year, film_rating, url_image, film_thumbnail,
-                film_genre, film_description)
+        current_result = get_current_film_info_from_command(current_film_rate)
+        return current_result
 
     elif from_module == 'high':
         """ Берем информацию для реализации команды /high """
         current_film_rate = max(films, key=lambda x: x['rating'])
-        film_name = current_film_rate['title']
-        film_year = current_film_rate['year']
-        film_rating = current_film_rate['rating']
-        url_image = current_film_rate['image']
-        film_thumbnail = current_film_rate['thumbnail']
-        film_genre = current_film_rate['genre']
-        film_description = current_film_rate['description']
-        return (film_name, film_year, film_rating, url_image, film_thumbnail,
-                film_genre, film_description)
+        current_result = get_current_film_info_from_command(current_film_rate)
+        return current_result
 
     elif from_module == 'definite_year':
         """ Берем информацию для реализации команды /definite_year """
